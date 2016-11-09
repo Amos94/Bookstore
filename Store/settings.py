d0da2bcc98e8db732ac9bb69172f7c55b41c09ae
Amos Madalin Neculau <amos.neculau@kcl.ac.uk>
2016-11-09 14:08:09 +0000

    Registration => on the way.

--- Store/settings.py
+++ Store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
     'BookStore',
 ]
 
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Registration
+ACCOUNT_ACTIVATION_DAYS = 7 #number of days an account activation will last
+REGISTRATION_AUTO_LOGIN = True #auto login activated
